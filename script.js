123f37677c5bcbb0036ba446c193dc103ffe7b9c
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-05-11 09:35:23 +0300

    added

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ const html = document.getElementById("html");
 console.log(toggletheme.checked)
 
 toggletheme.addEventListener("change",togglethemeHandler)
-
+// html.dataset.theme = "theme";
 function togglethemeHandler() {
     if(toggletheme.checked){
         html.dataset.theme = "dark";
+        localStorage.setItem('theme', 'dark');
     }else {
         html.dataset.theme = "light"; 
+        localStorage.setItem('theme', 'light');
     };
+}
+
+const theme = localStorage.getItem('theme');
+if(theme == 'light' || !theme){
+    html.dataset.theme = "light";
+}else{
+    html.dataset.theme = "dark";
+    document.getElementById("checkbox-header").checked = true;
 }
